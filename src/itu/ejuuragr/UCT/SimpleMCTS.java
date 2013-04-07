394725827c766ab188b00f569ba286308ad96ec5
rasmusgreve <ragr@itu.dk>
2013-04-08 00:24:10 +0200

    Minor changes

--- src/itu/ejuuragr/UCT/SimpleMCTS.java
+++ src/itu/ejuuragr/UCT/SimpleMCTS.java
@@ -26,7 +26,7 @@ import ch.idsia.mario.environments.Environment;
  */
 public class SimpleMCTS extends KeyAdapter implements MCTSAgent<UCTNode> {
 	
-	protected static int TIME_PER_TICK = 1000; // milliseconds
+	protected static int TIME_PER_TICK = 39; // milliseconds
 	public static int RANDOM_SAMPLES_LIMIT = 4;
 	protected static final double cp = 1.5/8;// 1.0/Math.sqrt(2); // 1.5/8;
 	
@@ -49,8 +49,8 @@ public class SimpleMCTS extends KeyAdapter implements MCTSAgent<UCTNode> {
 	
 	@Override
 	public boolean[] getAction(Environment obs) {
-		System.out.println("Mario y: " + obs.getMarioFloatPos()[1]);
-		MCTSTools.printObservation(obs.getLevelSceneObservationZ(0));
+		
+		//MCTSTools.printObservation(obs.getLevelSceneObservationZ(0));
 		int m = obs.getMarioMode();
 		if (m != lastMode)
 			MCTSTools.print("Mode changed: " + m);
