35068ad0d486889d2588aceadcd021a24967b351
rasmusgreve <rasmusgreve@gmail.com>
2013-04-15 12:16:00 +0200

    Added missing tests to SoftMax batch

--- SoftMax STATS.bat
+++ SoftMax STATS.bat
@@ -6,31 +6,43 @@ echo %DATE% %TIME%
 
 echo Testing Softmax, RSL:4 cp:0.1875 Q:0
 java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.1875 0 >> "SoftMax 4 0.1875 0.txt"
-echo Softmax done (1/5)
+echo Softmax done (1/7)
 
 echo %DATE% %TIME%
 
 echo Testing Softmax, RSL:4 cp:0.1875 Q:0.125
 java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.1875 0.125 >> "SoftMax 4 0.1875 0.125.txt"
-echo Softmax done (2/5)
+echo Softmax done (2/7)
 
 echo %DATE% %TIME%
 
 echo Testing Softmax, RSL:4 cp:0.1875 Q:0.25
 java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.1875 0.25 >> "SoftMax 4 0.1875 0.25.txt"
-echo Softmax done (3/5)
+echo Softmax done (3/7)
+
+echo %DATE% %TIME%
+
+echo Testing Softmax, RSL:4 cp:0.375 Q:0.25
+java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.375 0.25 >> "SoftMax 4 0.375 0.25.txt"
+echo Softmax done (4/7)
 
 echo %DATE% %TIME%
 
 echo Testing Softmax, RSL:4 cp:0.1875 Q:0.5
 java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.1875 0.5 >> "SoftMax 4 0.1875 0.5.txt"
-echo Softmax done (4/5)
+echo Softmax done (5/7)
+
+echo %DATE% %TIME%
+
+echo Testing Softmax, RSL:4 cp:0.75 Q:0.5
+java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.75 0.5 >> "SoftMax 4 0.75 0.5.txt"
+echo Softmax done (6/7)
 
 echo %DATE% %TIME%
 
 echo Testing Softmax, RSL:4 cp:0.1875 Q:1
 java itu.ejuuragr.MiniStats itu.ejuuragr.softmax.SoftMaxMCTS 0 4 0.1875 1 >> "SoftMax 4 0.1875 1.txt"
-echo Softmax done (5/5)
+echo Softmax done (7/7)
 
 echo %DATE% %TIME%
 echo ALL DONE
