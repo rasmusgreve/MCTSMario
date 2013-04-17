00269e0a62cce8f8671ec789d17fed4d935b269d
rasmusgreve <rasmusgreve@gmail.com>
2013-04-17 12:00:42 +0200

    Added MacroActions to batch

--- Enhancements STATS.bat
+++ Enhancements STATS.bat
@@ -6,25 +6,31 @@ echo %DATE% %TIME%
 
 echo Testing Limited Actions
 java itu.ejuuragr.MiniStats itu.ejuuragr.highdk.LimitedActions2 0 4 0.1875 0 >> "LimitedActions2.txt"
-echo Limited Actions done (1/4)
+echo Limited Actions done (1/5)
 
 echo %DATE% %TIME%
 
 echo Testing Hole Detection
 java itu.ejuuragr.MiniStats itu.ejuuragr.highdk.HoleDetection 0 4 0.1875 0 >> "HoleDetection.txt"
-echo Hole Detection done (2/4)
+echo Hole Detection done (2/5)
+
+echo %DATE% %TIME%
+
+echo Testing Macro Actions
+java itu.ejuuragr.MiniStats itu.ejuuragr.UCT.MacroActions 0 4 0.1875 0 >> "MacroActions.txt"
+echo Hole Detection done (3/5)
 
 echo %DATE% %TIME%
 
 echo Testing H. Partial Expansion
 java itu.ejuuragr.MiniStats itu.ejuuragr.heuristicpartial.HeuristicPartialMCTS 0 4 0.1875 0 >> "HPartialExpansion.txt"
-echo H. Partial Expansion done (3/4)
+echo H. Partial Expansion done (4/5)
 
 echo %DATE% %TIME%
 
 echo Testing Checkpoints
 java itu.ejuuragr.MiniStats itu.ejuuragr.checkpoints.CheckpointUCT 0 4 0.1875 0 >> "Checkpoints.txt"
-echo Checkpoints done (4/4)
+echo Checkpoints done (5/5)
 
 echo %DATE% %TIME%
 echo ALL DONE
