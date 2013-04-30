4e36c7225fd54aef62cd151e887f57815cd570a6
rasmusgreve <rasmusgreve@gmail.com>
2013-04-30 17:28:53 +0200

    Small changes + java behaviour test

--- src/itu/ejuuragr/UCT/EnhancementTesterNode.java
+++ src/itu/ejuuragr/UCT/EnhancementTesterNode.java
@@ -115,7 +115,7 @@ public class EnhancementTesterNode extends UCTNode {
 			double curScore;
 			if(children[i] != null){
 				curScore = children[i].calculateConfidence(cp);
-			}else if(actionScores[i] != 0){ // the difference
+			}else if(actionScores[i] != 0){ 
 				curScore = newScore;
 			}else{
 				continue;
@@ -146,7 +146,7 @@ public class EnhancementTesterNode extends UCTNode {
 	}
 	
 	private double calculateConfidenceNew(double cp){		
-		double exploitation = 0.55;
+		double exploitation = 0.0;
 		double exploration = cp*Math.sqrt(2*Math.log(this.visited)); // this is same has dividing by 1 because the new node has obviously never been visited
 		//System.out.printf("Exploit: %f Explore: %f\n", exploitation, exploration);
 		return exploitation + exploration;
