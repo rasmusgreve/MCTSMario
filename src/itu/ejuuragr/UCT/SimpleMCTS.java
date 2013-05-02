04445190d1f182f876037043730fdccf4867f4e0
EmilDesktop <emilovicmail@gmail.com>
2013-05-02 23:37:40 +0200

    some batch changes

--- src/itu/ejuuragr/UCT/SimpleMCTS.java
+++ src/itu/ejuuragr/UCT/SimpleMCTS.java
@@ -25,7 +25,7 @@ public class SimpleMCTS extends KeyAdapter implements MCTSAgent<UCTNode> {
 	
 	protected static int TIME_PER_TICK = 39; // milliseconds
 
-	public static int RANDOM_SAMPLES_LIMIT = 8;
+	public static int RANDOM_SAMPLES_LIMIT = 6;
 	public static double cp = 0.5;
 	private boolean SAVE_NEXT_TREE = false;
 	protected int maxDepth = 0;
