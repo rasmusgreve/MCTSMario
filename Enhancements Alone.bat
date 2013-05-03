0bd7923c2f3a20126bb4e39f19e0700acec9357d
nezbo <emilovicmail@gmail.com>
2013-05-03 14:12:45 +0200

    secured constants in batch

--- Enhancements Alone.bat
+++ Enhancements Alone.bat
@@ -6,37 +6,37 @@ REM class seed rsl cp USE_SOFTMAX USE_MACRO_ACTIONS USE_PARTIAL_EXPANSION USE_RO
 
 echo %DATE% %TIME%
 echo Testing Softmax
-java itu.ejuuragr.MiniStats2 itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 1 0 0 0 0 0 >> "Enhancement Softmax.txt"
+java itu.ejuuragr.HardStats itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 1 0 0 0 0 0 >> "Enhancement Softmax.txt"
 echo Completed test (1/6)
 
 echo %DATE% %TIME%
 
 echo Testing Macro
-java itu.ejuuragr.MiniStats2 itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 1 0 0 0 0 >> "Enhancement Macro.txt"
+java itu.ejuuragr.HardStats itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 1 0 0 0 0 >> "Enhancement Macro.txt"
 echo Completed test (2/6)
 
 echo %DATE% %TIME%
 
 echo Testing Partial
-java itu.ejuuragr.MiniStats2 itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 1 0 0 0 >> "Enhancement Partial.txt"
+java itu.ejuuragr.HardStats itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 1 0 0 0 >> "Enhancement Partial.txt"
 echo Completed test (3/6)
 
 echo %DATE% %TIME%
 
 echo Testing Roulette
-java itu.ejuuragr.MiniStats2 itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 0 1 0 0 >> "Enhancement Roulette.txt"
+java itu.ejuuragr.HardStats itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 0 1 0 0 >> "Enhancement Roulette.txt"
 echo Completed test (4/6)
 
 echo %DATE% %TIME%
 
 echo Testing Hole
-java itu.ejuuragr.MiniStats2 itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 0 0 1 0 >> "Enhancement Hole.txt"
+java itu.ejuuragr.HardStats itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 0 0 1 0 >> "Enhancement Hole.txt"
 echo Completed test (5/6)
 
 echo %DATE% %TIME%
 
 echo Testing Limited
-java itu.ejuuragr.MiniStats2 itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 0 0 0 1 >> "Enhancement Limited.txt"
+java itu.ejuuragr.HardStats itu.ejuuragr.UCT.EnhancementTester 0 6 0.25 0.125 0 0 0 0 0 1 >> "Enhancement Limited.txt"
 echo Completed test (6/6)
 
 echo %DATE% %TIME%
