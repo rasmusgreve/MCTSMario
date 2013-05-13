c9ef31d509149a3c397ad32527c76e52ac340214
nezbo <emilovicmail@gmail.com>
2013-05-13 11:08:52 +0200

    derp

--- src/ch/idsia/scenarios/Play.java
+++ src/ch/idsia/scenarios/Play.java
@@ -30,9 +30,10 @@ public class Play {
         options.setNumberOfTrials(1);
         options.setMatlabFileName("");
         options.setLevelRandSeed((int) (Math.random () * Integer.MAX_VALUE));
+        options.setLevelRandSeed(131626294);
         //options.setLevelRandSeed(1337);
-        options.setLevelLength(1000);
-        
+        //options.setLevelLength(1500);
+       
         options.setLevelDifficulty(20);
         task.setOptions(options);
         System.out.println("Seed: " + options.getLevelRandSeed());
